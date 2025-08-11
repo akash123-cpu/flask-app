90d7214afa36a6b34c50f28c09d28242a9f92c5d
akash123-cpu <akash1474@gmail.com>
2025-08-11 19:37:41 +0530

    Replace old files with new one

--- testt.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('1C9DC2D6B329_enriched.csv', low_memory=False)
-df['date_time'] = pd.to_datetime(df['date_time'], format="%d/%m/%y %H:%M", errors='coerce')
-print(df.head())
-print(df['date_time'].min(), df['date_time'].max())
